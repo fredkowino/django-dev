43a07b8932f3d284b60c235810212d619330a6c1
fredrick Oduor <kowino.fred@gmail.com>
2022-04-01 18:35:30 +0300

    for loop to test range of values.

--- test.py
+++ test.py
@@ -2,3 +2,8 @@ print('print git command')
 print("This seems interesting")
 
 # This is working out well. this is a comment
+for i in range (1, 10):
+    if i < 9:
+        print('this is i value')
+    else:
+        print('The test failed.')
